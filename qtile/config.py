c7271c28e7bc1c578f44fdbe9f5ce5fadc7b66ed
Samuel T. Wallace <samueltwallace@gmail.com>
2020-06-24 09:33:34 -0400

    polybar colors

---
+++ qtile/config.py
@@ -0,0 +1,21 @@
+from libqtile.config import Key, Screen, Group, Drag, Click
+from libqtile.command import lazy
+from libqtile import layout, bar, widget, hook
+
+mod = "mod4"
+
+keys = [
+        Key([mod,"shift"], "c", lazy.restart()),
+        Key([mod],"Return", lazy.spawn("alacritty")),
+        Key([mod,"shift"], "Return", lazy.spawn("gtk-launch $(xdg-settings get default-web-browser)")),
+        Key([mod,"shift"],"q",lazy.window.kill()),
+        Key([],"XF86AudioRaiseVolume",lazy.spawn("pactl set-sink-volume @DEFAULT_SINK@ +5%")) #And So on...
+
+
+
+
+
+
+
+
+        ]
